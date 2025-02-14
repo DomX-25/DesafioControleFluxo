ba1e28f205a471ed5243d9d4f92f75ded56f66d4
Dom x Teste 2 <alejandro.jonathan@hotmail.com>
2025-02-14 20:46:07 +0000

    Teste Concluido

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
+@SuppressWarnings("unused")
 public class Contador {
 	public static void main(String[] args) {
-		try (Scanner terminal = new Scanner(System.in)) {
-			System.out.println("Digite o primeiro parâmetro");
-			int parametroUm = terminal.nextInt(7);
-			System.out.println("Digite o segundo parâmetro");
-			int parametroDois = terminal.nextInt(35);
+            int parametroUm= 34;
+            int parametroDois = 92;
+
+		try {
+            contar (parametroUm, parametroDois);
+              } catch (Exception e) {
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");               }
 			
 			try {
 				//chamando o método contendo a lógica de contagem
@@ -18,9 +21,8 @@ public class Contador {
 			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 				//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 			}
-		}
-	}
-	
+                        }
+                        	
 	static void contar(int parametroUm, int parametroDois ) throws Exception {
 		if (parametroUm >= parametroDois) {
 			throw new Exception("O segundo parâmetro deve ser maior que o primeiro");
